7e98c6050f749db8e198d5a669c8e783f79bf8a9
Yuechen Li <109704248+yuechen-li-dev@users.noreply.github.com>
2026-04-11 11:15:21 -0700

    Add ASSERT_NEAR and benchmark support to Marionette harness

--- tests/Marionette/test_main.cpp
+++ tests/Marionette/test_main.cpp
@@ -4,6 +4,18 @@
 
 int main(int argc, char** argv)
 {
+    if (argc >= 2 && argv[1] != nullptr) {
+        const std::string_view mode = argv[1];
+        if (mode == "--bench") {
+            std::string_view filter;
+            if (argc >= 3 && argv[2] != nullptr) {
+                filter = argv[2];
+            }
+
+            return ::marionette::tests::RunBenchmarks(filter);
+        }
+    }
+
     std::string_view filter;
     if (argc >= 2 && argv[1] != nullptr) {
         filter = argv[1];
